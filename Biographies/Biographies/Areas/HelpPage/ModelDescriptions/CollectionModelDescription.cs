fc2799fd7258edff9fcb47c2671ef212e0e33a16
freddyiniguez <iniguez.devt@gmail.com>
2019-07-01 16:06:53 -0500

    Start tracking project with default files and resources.

---
+++ Biographies/Biographies/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Biographies.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Biographies/Biographies/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Biographies.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
