3b3c296b0f7757f0a1a5e41e2ad24678761f8aa0
freddyiniguez <iniguez.devt@gmail.com>
2019-07-02 11:31:20 -0500

    Successfully retrieving candidates from database.

--- Biographies/Biographies/Infrastructure/Concrete/EFCandidatesRepository.cs
+++ Biographies/Biographies/Infrastructure/Concrete/EFCandidatesRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
 using Biographies.Entities;
 using Biographies.Infrastructure.Abstract;
 using Biographies.Models;
@@ -7,25 +9,40 @@ namespace Biographies.Infrastructure.Concrete
 {
     public class EFCandidatesRepository : ICandidatesRepository
     {
-        BiosEntities _Context = new BiosEntities();
+        /// <summary>
+        /// Variables
+        /// </summary>
+        BiosEntities _Context;
+        IMapper _IMapper;
 
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public EFCandidatesRepository()
+        {
+            _Context = new BiosEntities();
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<sp_bios_candidates_get_Result, CandidateModel>();
+            });
+
+            _IMapper = config.CreateMapper();
+
+        }
+
+
+        /// <summary>
+        /// GetCandidates()
+        /// This method retrieves all registered active candidates.
+        /// </summary>
+        /// <returns></returns>
         public List<CandidateModel> GetCandidates()
         {
-            List<CandidateModel> Candidates = new List<CandidateModel>()
-            {
-                new CandidateModel()
-                {
-                    IdCandidate = 1,
-                    FirstName = "Freddy",
-                    LastName = "Iñiguez",
-                    SecondLastName = "López",
-                    BirthDate = System.DateTime.Now,
-                    Email = "iniguez.devt@gmail.com",
-                    Phone = "3312362006",
-                    Cellphone = "3331143453"
-                }
-            };
+            var _SpResponse = _Context.sp_bios_candidates_get().ToList();
+
+            List<CandidateModel> Candidates = _IMapper.Map<List<sp_bios_candidates_get_Result>, List<CandidateModel>>(_SpResponse);
 
             return Candidates;
         }
